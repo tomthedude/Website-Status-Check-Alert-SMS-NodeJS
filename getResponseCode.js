13077220ea8661af442e802f1dce2824711a2b1b
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-18 22:39:51 +0300

    env

--- getResponseCode.js
+++ getResponseCode.js
@@ -4,43 +4,49 @@ dotenv.config();
 
 const WEBSITE_URL = process.env.WEBSITE_URL;
 const CHECK_INTERVAL_WHEN_OK = process.env.CHECK_INTERVAL_WHEN_OK;
+const CHECK_INTERVAL_WHEN_ERROR = process.env.CHECK_INTERVAL_WHEN_ERROR;
+const SENDER_NUMBER = process.env.SENDER_NUMBER;
 
 let interval = CHECK_INTERVAL_WHEN_OK;
 
 checkWebsite(WEBSITE_URL);
-//sendSMS();
-setInterval(() => {
-  result = checkWebsite(WEBSITE_URL);
-  if (result != 200) {
-    console.log('error '+result);
-  } else {
-    console.log('alles ok');
-  }
-},interval * 1000);
 
 
-function checkWebsite(url) {
-  return https.get(url, (res) => {
+function checkWebsite(url, interval) {
+  https.get(url, (res) => {
     console.log('statusCode:', res.statusCode);
-    return res.statusCode;
+    if (res.statusCode == "200") {
+      console.log("alles ok");
+      interval = CHECK_INTERVAL_WHEN_OK;
+    } else {
+      sendSMS(res.statusCode);
+      sendScreenshot(res);
+      console.log('error' + res.statusCode);
+      interval = CHECK_INTERVAL_WHEN_ERROR;
+    }
+    setTimeout(() => {
+      checkWebsite(url, interval);
+    }, interval * 1000);
     //console.log('headers:', res.headers);
-  
-    res.on('data', (d) => {
-      //process.stdout.write(d);
-    });
-  
   }).on('error', (e) => {
     console.error(e);
   });  
 }
 
-function sendSMS() {
+function sendScreenshot(res, mail){
+      res.on('data', (d) => {
+      //process.stdout.write(d);
+        console.log('screen shot sent to mail');
+    });
+}
+
+function sendSMS(reciever, text) {
   let plivo = require('plivo');
   let client = new plivo.Client();
   client.messages.create(
-    '+13322326084',
-    '+972556665917',
-    'בדיקה'
+    SENDER_NUMBER,
+    reciever,
+    text
   ).then(function(message_created) {
     console.log(message_created)
   });
