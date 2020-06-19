d10fa519639af893aac76446edd51f358278598d
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-19 18:15:47 +0300

    try using blessed

--- src/Tools/Main.js
+++ src/Tools/Main.js
@@ -1,5 +1,5 @@
 module.exports = class Main {
-  constructor(url) {
+  constructor(url, statsTable) {
     this.settings = require("../settings");
     this.totalStatusTimeSeconds = 0;
     this.latestStatus = 200;
@@ -9,6 +9,28 @@ module.exports = class Main {
     this.notifier = new notifierClass(url);
     this.prettyMilliseconds = require("pretty-ms");
     this.https = require("https");
+    this.currentStatusStats = { url: url, statusCode: 200, duration: 0 };
+      this.tableObject = {};
+      this.url = url;
+      this.statsTable = statsTable;
+      var blessed = require('blessed');
+
+      // Create a screen object.
+      var screen = blessed.screen({
+        smartCSR: true
+      });
+      
+            screen.title = 'my window title';
+      this.table = blessed.table();
+      this.table.setData(this.statsTable.dispalyTable);
+      screen.append(this.table);
+  }
+  addEntryToTable() {
+    this.statsTable.dispalyTable.push([Date.now(), this.url, this.statusCode, this.humanReadableStatusDuration]);
+  }
+  showStats() {
+      this.statsTable.dispalyTable[0] = [Date.now(), this.url, this.statusCode, this.humanReadableStatusDuration];
+      this.table.setData(this.statsTable.dispalyTable);
   }
   checkWebsite(url, interval) {
     this.https
@@ -18,34 +40,38 @@ module.exports = class Main {
         this.humanReadableStatusDuration = this.prettyMilliseconds(
           this.totalStatusTimeSeconds * 1000
         );
-        console.log("statusCode:", res.statusCode);
+        this.currentStatusStats.duration = this.humanReadableStatusDuration;
+        //console.log("statusCode:", res.statusCode);
         if (res.statusCode == "200") {
-          console.log("alles ok, " + url);
+          //console.log("alles ok, " + url);
           interval = this.settings.CHECK_INTERVAL_OK;
         } else {
           interval = this.settings.CHECK_INTERVAL_ERROR;
           this.notifier.notify(res, this.humanReadableStatusDuration);
         }
-        console.log(
-          `total status time: ${this.humanReadableStatusDuration}, url: ${url}`
-        );
+        // console.log(
+        //   `total status time: ${this.humanReadableStatusDuration}, url: ${url}`
+        // );
+        this.showStats();
         setTimeout(() => {
           this.checkWebsite(url, interval);
         }, interval * 1000);
         //console.log('headers:', res.headers);
       })
       .on("error", (e) => {
-        console.log("error with get, url: " + url);
+        console.log("error with get, stopping checks, url: " + url);
         //console.error(e);
       });
   }
 
   handleTiming(interval) {
     if (this.latestStatus != this.statusCode) {
+      this.addEntryToTable();
       this.totalStatusTimeSeconds = 0;
     } else {
       this.totalStatusTimeSeconds += interval; //total minutes
     }
+    this.currentStatusStats.statusCode = this.statusCode;
     this.latestStatus = this.statusCode;
   }
 };
--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const settings = require("./settings");
 let mainAppClass = require("./Tools/Main");
 let interval = settings.CHECK_INTERVAL_OK;
 
+class dispalyTable{
+  constructor() {
+    this.dispalyTable = [["time", "website", "status code", "duration"]];
+  }
+}
+
+let statsTable = new dispalyTable();
+
 // get websites from json file
 var websitesObject = JSON.parse(
   fs.readFileSync(settings.WEBSITES_FROM_FILE, "utf8")
@@ -17,6 +25,6 @@ for (url in websitesObject) {
   if (websitesObject[url] != true) {
     continue;
   }
-  let mainApp = new mainAppClass(url);
+  let mainApp = new mainAppClass(url, statsTable);
   mainApp.checkWebsite(url, interval);
 }
