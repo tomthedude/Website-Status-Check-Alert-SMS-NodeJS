4265768fa892730f88e88e7116a98d559d2f34e6
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-19 17:26:10 +0300

    formatting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require("fs");
 
 // init
 const settings = require("./settings");
@@ -6,7 +6,9 @@ let mainAppClass = require("./Tools/Main");
 let interval = settings.CHECK_INTERVAL_OK;
 
 // get websites from json file
-var websitesObject = JSON.parse(fs.readFileSync(settings.WEBSITES_FROM_FILE, 'utf8'));
+var websitesObject = JSON.parse(
+  fs.readFileSync(settings.WEBSITES_FROM_FILE, "utf8")
+);
 // add website from .env
 websitesObject[settings.WEBSITE_URL] = true;
 
