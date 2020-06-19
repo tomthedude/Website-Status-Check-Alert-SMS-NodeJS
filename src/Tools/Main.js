eb8852bcbc7b64d778d4269fca7dce522d0ce652
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-19 19:00:05 +0300

    remove console.log and show stats in table

--- src/Tools/Main.js
+++ src/Tools/Main.js
@@ -9,35 +9,63 @@ module.exports = class Main {
     this.notifier = new notifierClass(url);
     this.prettyMilliseconds = require("pretty-ms");
     this.https = require("https");
+    this.responseTimes = [];
+    this.url = url;
   }
   checkWebsite(url, interval) {
+    var responseTimeStart = new Date();
     this.https
       .get(url, (res) => {
         this.statusCode = res.statusCode;
+        this.handleResponseTime(
+          new Date() - responseTimeStart,
+          this.statusCode
+        );
         this.handleTiming(interval);
         this.humanReadableStatusDuration = this.prettyMilliseconds(
           this.totalStatusTimeSeconds * 1000
         );
-        console.log("statusCode:", res.statusCode);
         if (res.statusCode == "200") {
-          console.log("alles ok, " + url);
           interval = this.settings.CHECK_INTERVAL_OK;
         } else {
           interval = this.settings.CHECK_INTERVAL_ERROR;
           this.notifier.notify(res, this.humanReadableStatusDuration);
         }
-        console.log(
-          `total status time: ${this.humanReadableStatusDuration}, url: ${url}`
-        );
+        this.showStats();
         setTimeout(() => {
           this.checkWebsite(url, interval);
         }, interval * 1000);
-        //console.log('headers:', res.headers);
       })
       .on("error", (e) => {
         console.log("error with get, url: " + url);
-        //console.error(e);
       });
+  }
+
+  avgResponseTime() {
+    let totalTime = 0;
+    this.responseTimes.forEach(function (responseTimeObject) {
+      totalTime += responseTimeObject.responseTime;
+    });
+    return this.prettyMilliseconds(totalTime / this.responseTimes.length);
+  }
+
+  showStats() {
+    console.table({
+      time: new Date().toTimeString(),
+      URL: this.url,
+      statusCode: this.statusCode,
+      "status duration": this.humanReadableStatusDuration,
+      responseTime: this.prettyMilliseconds(this.responseTimes[0].responseTime),
+      "avg response time": this.avgResponseTime(),
+    });
+  }
+
+  handleResponseTime(responseTime, statusCode) {
+    this.responseTimes.unshift({
+      statusCode: statusCode,
+      responseTime: responseTime,
+      timestamp: new Date(),
+    });
   }
 
   handleTiming(interval) {
