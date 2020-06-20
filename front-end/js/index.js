3dc1be8be14811fc31bb621a56f9d6036c731e74
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-21 00:52:27 +0300

    ajax and loop

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,73 +1,6 @@
 if (findGetParameter("site") === null) {
-  $.ajax({
-    url: "/get-all-websites",
-    method: "GET",
-    type: "text/json",
-  })
-    .always(function () {
-      //$(button).html("Load Site Data...");
-    })
-    .done(function (evt) {
-      var result = JSON.parse(evt);
-      const ordered = {};
-      Object.keys(result)
-        .forEach(function (key) {
-          ordered[result[key].URL] = {};
-          ordered[result[key].URL][key] = result[key];
-        });
-      let toShow = {};
-      console.log(ordered);
-      Object.keys(ordered).forEach(function (key) {
-        toShow[key] = ordered[key][Object.keys(ordered[key])[0]];
-        console.log(toShow);
-      });
-      let html =
-        '<table class="table">' +
-        "<thead>" +
-        "<tr>" +
-        '<th scope="col">Domain</th>' +
-        '<th scope="col">Time</th>' +
-        '<th scope="col">Status Code</th>' +
-        '<th scope="col">Status Duration</th>' +
-        '<th scope="col">Response Time</th>' +
-        '<th scope="col">Response Time Avg</th>' +
-        "</tr>" +
-        "</thead>" +
-        "<tbody>";
-      for(key in toShow){
-        var dot = toShow[key].statusCode == 200 ? "greenDot" : "redDot";
-        html += '<div class="tables-doctor-content">';
-          html +=
-            "<tr>" +
-            "<td>" +
-            "<div style='display: flex;'><h2>" +
-            toShow[key].URL +
-            "</h2><h2 class='dot " +
-            dot +
-            "'></h2></div>" +
-            "</td>" +
-            "<td>" +
-            toShow[key].time +
-            "</td>" +
-            "<td>" +
-            toShow[key].statusCode +
-            "</td>" +
-            "<td>" +
-            toShow[key]["status duration"] +
-            "</td>" +
-            "<td>" +
-            toShow[key].responseTime +
-            "</td>" +
-            "<td>" +
-            toShow[key]["avg response time"] +
-            "</td>" +
-            "</tr>";
-
-        };
-        html += "</tbody></table>";
-        html += "</div>";
-        $(".tables-doctor").html(html);
-    });
+    indexPageAjax();
+    indexPageLoop();
 } else {
   // json data
   var api = "/" + findGetParameter("site"); //httpslowchost.co.il.json
@@ -178,4 +111,82 @@ function getWebsiteName() {
   var formatted = unformatted.replace("https", "https://");
   formatted = formatted.replace(".json", "");
   return formatted;
+}
+
+function indexPageAjax() {
+    $.ajax({
+        url: "/get-all-websites",
+        method: "GET",
+        type: "text/json",
+      })
+        .always(function () {
+          //$(button).html("Load Site Data...");
+        })
+        .done(function (evt) {
+          var result = JSON.parse(evt);
+          const ordered = {};
+          Object.keys(result)
+            .forEach(function (key) {
+              ordered[result[key].URL] = {};
+              ordered[result[key].URL][key] = result[key];
+            });
+          let toShow = {};
+          console.log(ordered);
+          Object.keys(ordered).forEach(function (key) {
+            toShow[key] = ordered[key][Object.keys(ordered[key])[0]];
+            console.log(toShow);
+          });
+          let html =
+            '<table class="table">' +
+            "<thead>" +
+            "<tr>" +
+            '<th scope="col">Domain</th>' +
+            '<th scope="col">Time</th>' +
+            '<th scope="col">Status Code</th>' +
+            '<th scope="col">Status Duration</th>' +
+            '<th scope="col">Response Time</th>' +
+            '<th scope="col">Response Time Avg</th>' +
+            "</tr>" +
+            "</thead>" +
+            "<tbody>";
+          for(key in toShow){
+            var dot = toShow[key].statusCode == 200 ? "greenDot" : "redDot";
+            html += '<div class="tables-doctor-content">';
+              html +=
+                "<tr>" +
+                "<td>" +
+                "<div style='display: flex;'><h2>" +
+                toShow[key].URL +
+                "</h2><h2 class='dot " +
+                dot +
+                "'></h2></div>" +
+                "</td>" +
+                "<td>" +
+                toShow[key].time +
+                "</td>" +
+                "<td>" +
+                toShow[key].statusCode +
+                "</td>" +
+                "<td>" +
+                toShow[key]["status duration"] +
+                "</td>" +
+                "<td>" +
+                toShow[key].responseTime +
+                "</td>" +
+                "<td>" +
+                toShow[key]["avg response time"] +
+                "</td>" +
+                "</tr>";
+    
+            };
+            html += "</tbody></table>";
+            html += "</div>";
+            $(".tables-doctor").html(html);
+        });
+}
+
+function indexPageLoop() {
+    setInterval(function () {
+        indexPageAjax();
+    }, 60000);
 }
