5f220fa710fadbf6287848366da9bf544477b089
Tom Yoshai <tom.yoshai@sergata.com>
2020-06-21 00:46:48 +0300

    include index results

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,89 +1,165 @@
-// json data
-var api = "/" + findGetParameter("site"); //httpslowchost.co.il.json
-// button trigger
-$("#view-doctors").on("click", function () {
-  var button = $(this);
+if (findGetParameter("site") === null) {
   $.ajax({
-    url: api,
+    url: "/get-all-websites",
     method: "GET",
     type: "text/json",
   })
     .always(function () {
-      $(button).html("Load Site Data...");
+      //$(button).html("Load Site Data...");
     })
     .done(function (evt) {
-      // Disable button
-      $(button).prop("disabled", true);
-      // Set timeout for lazy loading
-      let interval = 0;
-      setTimeout(function () {
-        var result = JSON.parse(evt);
-        const ordered = {};
-        Object.keys(result)
-          .reverse()
-          .forEach(function (key) {
-            ordered[key] = result[key];
-          });
-        var dot = ordered[Object.keys(ordered)[0]].statusCode == 200 ? "greenDot" : "redDot";
-        var html =
-          "<div style='display: flex;'><h2>" +
-          getWebsiteName() +
-          "</h2><h2 class='dot " +
-          dot +
-          "'></h2></div>";
+      var result = JSON.parse(evt);
+      const ordered = {};
+      Object.keys(result)
+        .reverse()
+        .forEach(function (key) {
+          ordered[result[key].URL] = {};
+          ordered[result[key].URL][key] = result[key];
+        });
+      let toShow = {};
+      console.log(ordered);
+      Object.keys(ordered).forEach(function (key) {
+        toShow[key] = ordered[key][Object.keys(ordered[key])[0]];
+        console.log(toShow);
+      });
+      let html =
+        '<table class="table">' +
+        "<thead>" +
+        "<tr>" +
+        '<th scope="col">Domain</th>' +
+        '<th scope="col">Time</th>' +
+        '<th scope="col">Status Code</th>' +
+        '<th scope="col">Status Duration</th>' +
+        '<th scope="col">Response Time</th>' +
+        '<th scope="col">Response Time Avg</th>' +
+        "</tr>" +
+        "</thead>" +
+        "<tbody>";
+      for(key in toShow){
+        var dot = toShow[key].statusCode == 200 ? "greenDot" : "redDot";
         html += '<div class="tables-doctor-content">';
-        html +=
-          '<table class="table">' +
-          "<thead>" +
-          "<tr>" +
-          '<th scope="col">Time</th>' +
-          '<th scope="col">Status Code</th>' +
-          '<th scope="col">Status Duration</th>' +
-          '<th scope="col">Response Time</th>' +
-          '<th scope="col">Response Time Avg</th>' +
-          "</tr>" +
-          "</thead>" +
-          "<tbody>";
-        for (var i in ordered) {
           html +=
             "<tr>" +
             "<td>" +
-            ordered[i].time +
+            "<div style='display: flex;'><h2>" +
+            toShow[key].URL +
+            "</h2><h2 class='dot " +
+            dot +
+            "'></h2></div>" +
             "</td>" +
             "<td>" +
-            ordered[i].statusCode +
+            toShow[key].time +
             "</td>" +
             "<td>" +
-            ordered[i]["status duration"] +
+            toShow[key].statusCode +
             "</td>" +
             "<td>" +
-            ordered[i].responseTime +
+            toShow[key]["status duration"] +
             "</td>" +
             "<td>" +
-            ordered[i]["avg response time"] +
+            toShow[key].responseTime +
+            "</td>" +
+            "<td>" +
+            toShow[key]["avg response time"] +
             "</td>" +
             "</tr>";
-        }
+
+        };
         html += "</tbody></table>";
         html += "</div>";
-        // Set all content
         $(".tables-doctor").html(html);
-      }, 1000);
-    })
-    .fail(function () {
-      alert("Error : Failed to reach API Url or check your connection");
-      $(button).prop("disabled", false);
-    })
-    .then(function (evt) {
-      setTimeout(function () {
-        $(button).css({ "background-color": "#ccc" }).hide();
-      }, 1000);
     });
-});
-$("#view-doctors").trigger("click");
-setInterval(function () {
+} else {
+  // json data
+  var api = "/" + findGetParameter("site"); //httpslowchost.co.il.json
+  // button trigger
+  $("#view-doctors").on("click", function () {
+    var button = $(this);
+    $.ajax({
+      url: api,
+      method: "GET",
+      type: "text/json",
+    })
+      .always(function () {
+        $(button).html("Load Site Data...");
+      })
+      .done(function (evt) {
+        // Disable button
+        $(button).prop("disabled", true);
+        // Set timeout for lazy loading
+        let interval = 0;
+        setTimeout(function () {
+          var result = JSON.parse(evt);
+          const ordered = {};
+          Object.keys(result)
+            .reverse()
+            .forEach(function (key) {
+              ordered[key] = result[key];
+            });
+          var dot =
+            ordered[Object.keys(ordered)[0]].statusCode == 200
+              ? "greenDot"
+              : "redDot";
+          var html =
+            "<div style='display: flex;'><h2>" +
+            getWebsiteName() +
+            "</h2><h2 class='dot " +
+            dot +
+            "'></h2></div>";
+          html += '<div class="tables-doctor-content">';
+          html +=
+            '<table class="table">' +
+            "<thead>" +
+            "<tr>" +
+            '<th scope="col">Time</th>' +
+            '<th scope="col">Status Code</th>' +
+            '<th scope="col">Status Duration</th>' +
+            '<th scope="col">Response Time</th>' +
+            '<th scope="col">Response Time Avg</th>' +
+            "</tr>" +
+            "</thead>" +
+            "<tbody>";
+          for (var i in ordered) {
+            html +=
+              "<tr>" +
+              "<td>" +
+              ordered[i].time +
+              "</td>" +
+              "<td>" +
+              ordered[i].statusCode +
+              "</td>" +
+              "<td>" +
+              ordered[i]["status duration"] +
+              "</td>" +
+              "<td>" +
+              ordered[i].responseTime +
+              "</td>" +
+              "<td>" +
+              ordered[i]["avg response time"] +
+              "</td>" +
+              "</tr>";
+          }
+          html += "</tbody></table>";
+          html += "</div>";
+          // Set all content
+          $(".tables-doctor").html(html);
+        }, 1000);
+      })
+      .fail(function () {
+        alert("Error : Failed to reach API Url or check your connection");
+        $(button).prop("disabled", false);
+      })
+      .then(function (evt) {
+        setTimeout(function () {
+          $(button).css({ "background-color": "#ccc" }).hide();
+        }, 1000);
+      });
+  });
   $("#view-doctors").trigger("click");
-}, 60000);
+  setInterval(function () {
+    $("#view-doctors").trigger("click");
+  }, 60000);
+}
 
 function findGetParameter(parameterName) {
   var result = null,
--- src/Tools/ResultsServer.js
+++ src/Tools/ResultsServer.js
@@ -6,26 +6,48 @@ module.exports = class ResultsServer {
 
     http
       .createServer((req, res) => {
-        var folder = this.settings.FRON_END_FOLDER;
-        var formattedPage = req.url;
-        if (
-          req.url.includes(".json") &&
-          req.url.toLowerCase().indexOf("?") === -1
-        ) {
-          folder = this.settings.RESULTS_FOLDER;
-        } else if (req.url.toLowerCase().indexOf("?") > 0) {
-          var formattedPage = req.url.substring(0, req.url.indexOf("?"));
-        }
-        fs.readFile(folder + formattedPage, function (err, data) {
-          if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
+        if (req.url.includes("get-all-websites")) {
+          var jsonConcat = require("json-concat");
+
+          jsonConcat(
+            {
+              src: ["results/"],
+              dest: "front-end/aggregated.json",
+            },
+             (json) =>  {
+              fs.readFile("front-end/aggregated.json", function (err, data) {
+                if (err) {
+                  res.writeHead(404);
+                  res.end(JSON.stringify(err));
+                  return;
+                }
+                res.writeHead(200);
+                res.end(data);
+              });
+            }
+          );
+        } else {
+          var folder = this.settings.FRON_END_FOLDER;
+          var formattedPage = req.url;
+          if (
+            req.url.includes(".json") &&
+            req.url.toLowerCase().indexOf("?") === -1
+          ) {
+            folder = this.settings.RESULTS_FOLDER;
+          } else if (req.url.toLowerCase().indexOf("?") > 0) {
+            var formattedPage = req.url.substring(0, req.url.indexOf("?"));
           }
-          res.writeHead(200);
-          res.end(data);
-        });
-        console.log("server started");
+          fs.readFile(folder + formattedPage, function (err, data) {
+            if (err) {
+              res.writeHead(404);
+              res.end(JSON.stringify(err));
+              return;
+            }
+            res.writeHead(200);
+            res.end(data);
+          });
+          console.log("server started");
+        }
       })
       .listen(8080);
   }
