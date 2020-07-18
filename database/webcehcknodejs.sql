56a9f34714ae889ebf56e8488f70cae20d9a6e92
Tom Yoshai <tom.yoshai@sergata.com>
2020-07-18 20:07:04 +0300

    add database

---
+++ database/webcehcknodejs.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 18, 2020 at 07:05 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webcehcknodejs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status logs`
+--
+
+CREATE TABLE `status logs` (
+  `id` int(11) NOT NULL,
+  `url` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status_code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_duration` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `response_time` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avg_response_time` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_cached` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `status logs`
+--
+ALTER TABLE `status logs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`,`timestamp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `status logs`
+--
+ALTER TABLE `status logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
