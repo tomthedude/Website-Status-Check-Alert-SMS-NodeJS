d359e18a047ddae2fa9b2b090789eff3f2830e49
Tom Yoshai <tom.yoshai@sergata.com>
2020-07-18 20:36:39 +0300

    run only if connected

--- src/Tools/MySql.js
+++ src/Tools/MySql.js
@@ -1,5 +1,6 @@
 module.exports =  class MySql{
-    constructor(host,user, password){
+    constructor(host, user, password) {
+        this.isConnected = false;
         var mysql = require('mysql');
 
         this.sqlCon = mysql.createConnection({
@@ -11,11 +12,16 @@ module.exports =  class MySql{
         this.sqlCon.connect(function(err) {
           if (err) throw err;
           // start sql logging flag
-          console.log("Connected To SQL!");
+            this.runQuery('use webcehcknodejs;');
+            this.isConnected = true;
+            console.log("Connected To SQL!");
         });
     }
 
     runQuery(sqlQurey) {
+        if (!this.isConnected) {
+            return;
+        }
         console.log("mysql query: " + sqlQurey);
         this.sqlCon.query(sqlQurey, function (err, result) {
           if (err) throw err;
